114261ece30ea56eeba1a9ede25a42c268c860fa
MostafaRahimi-7 <mostafarahimi.a229@gmail.com>
2024-07-24 18:11:46 +0430

    Adding files to the repository

---
+++ main.js
@@ -0,0 +1,64 @@
+/*
+console.log("Micheal");
+console.log(23);
+console.log(40 + 8 + 23 - 10);
+let firstName = "Omer";
+console.log(firstName);
+console.log(firstName);
+console.log(firstName);
+
+// Name the vairables perfectly:
+
+let firstJob = "Teacher";
+let curentJob = "Programmer";
+
+// Don't do like this:
+
+let job1 = "Teacher";
+let job2 = "Programmer";
+console.log(firstJob);
+console.log(curentJob);
+
+// Data Types:
+
+let age = 23;
+let lastName = "Rahimi";
+let isActive = true; // or flase
+let children; // don't assign values emty value
+let id = null; // it different with undefined
+const mySymbol = symbol(); // not realy useful.
+const bigNumber = BigInt(999999999999999); // for big integer number
+*/
+
+let appleIsFruit = true;
+console.log(appleIsFruit);
+
+console.log(typeof true);
+console.log(typeof "mostafa");
+console.log(typeof 2352);
+console.log(typeof "rahimi");
+
+appleIsFruit = "true";
+console.log(typeof appleIsFruit);
+
+// let _undefined;
+// console.log(_undefined);
+// console.log(typeof _undefined);
+
+let _undefined = 23;
+console.log(_undefined);
+console.log(typeof _undefined);
+
+console.log(typeof null);
+
+// Declaring the varibales:
+
+let price = 120;
+price = 250;
+
+const birthYear = 2001;
+// birthYear = 2000; this is an error
+console.log(birthYear);
+// const escape; this is also an error we have to assign a value to const varible after decalaring.
+
+console.log("There is some changes in this repository");
