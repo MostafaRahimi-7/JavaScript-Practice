dd67b0939c42699bcdddfd02d9ce44c7a7f4d00c
MostafaRahimi-7 <mostafarahimi.a229@gmail.com>
2024-07-24 18:47:08 +0430

    Learning the operators

--- main.js
+++ main.js
@@ -1,65 +1,124 @@
-/*
-console.log("Micheal");
-console.log(23);
-console.log(40 + 8 + 23 - 10);
-let firstName = "Omer";
-console.log(firstName);
-console.log(firstName);
-console.log(firstName);
+// /*
+// console.log("Micheal");
+// console.log(23);
+// console.log(40 + 8 + 23 - 10);
+// let firstName = "Omer";
+// console.log(firstName);
+// console.log(firstName);
+// console.log(firstName);
 
-// Name the vairables perfectly:
+// // Name the vairables perfectly:
 
-let firstJob = "Teacher";
-let curentJob = "Programmer";
+// let firstJob = "Teacher";
+// let curentJob = "Programmer";
 
-// Don't do like this:
+// // Don't do like this:
 
-let job1 = "Teacher";
-let job2 = "Programmer";
-console.log(firstJob);
-console.log(curentJob);
+// let job1 = "Teacher";
+// let job2 = "Programmer";
+// console.log(firstJob);
+// console.log(curentJob);
 
-// Data Types:
+// // Data Types:
 
-let age = 23;
-let lastName = "Rahimi";
-let isActive = true; // or flase
-let children; // don't assign values emty value
-let id = null; // it different with undefined
-const mySymbol = symbol(); // not realy useful.
-const bigNumber = BigInt(999999999999999); // for big integer number
-*/
+// let age = 23;
+// let lastName = "Rahimi";
+// let isActive = true; // or flase
+// let children; // don't assign values emty value
+// let id = null; // it different with undefined
+// const mySymbol = symbol(); // not realy useful.
+// const bigNumber = BigInt(999999999999999); // for big integer number
+// */
 
-let appleIsFruit = true;
-console.log(appleIsFruit);
+// let appleIsFruit = true;
+// console.log(appleIsFruit);
 
-console.log(typeof true);
-console.log(typeof "mostafa");
-console.log(typeof 2352);
-console.log(typeof "rahimi");
+// console.log(typeof true);
+// console.log(typeof "mostafa");
+// console.log(typeof 2352);
+// console.log(typeof "rahimi");
 
-appleIsFruit = "true";
-console.log(typeof appleIsFruit);
+// appleIsFruit = "true";
+// console.log(typeof appleIsFruit);
 
-// let _undefined;
+// // let _undefined;
+// // console.log(_undefined);
+// // console.log(typeof _undefined);
+
+// let _undefined = 23;
 // console.log(_undefined);
 // console.log(typeof _undefined);
 
-let _undefined = 23;
-console.log(_undefined);
-console.log(typeof _undefined);
+// console.log(typeof null);
 
-console.log(typeof null);
+// // Declaring the varibales:
 
-// Declaring the varibales:
+// let price = 120;
+// price = 250;
 
-let price = 120;
-price = 250;
+// const birthYear = 2001;
+// // birthYear = 2000; this is an error
+// console.log(birthYear);
+// // const escape; this is also an error we have to assign a value to const varible after decalaring.
 
-const birthYear = 2001;
-// birthYear = 2000; this is an error
-console.log(birthYear);
-// const escape; this is also an error we have to assign a value to const varible after decalaring.
+// // Arithmetic Operators:
+// const currentyear = 2024;
+// const age1 = currentyear - 2001;
+// const age2 = currentyear - 2000;
+// console.log(age1, age2);
 
-console.log("There is some changes in this repository");
-console.log("This is a change");
+// console.log(age1 * 16);
+// console.log(age1 / 10);
+// console.log(4 ** 5);
+
+// const firstName = "Mostafa";
+// const lastName = "Rahimi";
+// console.log(firstName + " " + lastName);
+
+// // Assignmen operators
+
+// let fullName = firstName + lastName;
+// console.log(fullName);
+
+// let counter = 0;
+// counter += 10;
+// console.log(counter);
+
+// counter *= 5;
+// console.log(counter);
+
+// counter /= 2;
+// console.log(counter);
+
+// counter++;
+// counter--;
+// counter--;
+// counter--;
+// console.log(counter);
+
+// // Comprasino operators:
+
+// console.log(age1 > age2);
+// console.log(age1 < age2);
+// console.log(age2 >= 18);
+// console.log(age2 <= 18);
+
+// const legalAge = age2 >= 18;
+// console.log(legalAge);
+
+// console.log(currentyear - 1996 > currentyear - 2005);
+
+const currentyear = 2024;
+const age1 = currentyear - 1996;
+const age2 = currentyear - 2005;
+
+console.log(currentyear - 1996 > currentyear - 2005);
+console.log(100 - 30 - 10);
+
+let a, b;
+a = b = 100 - 30 - 10;
+console.log(a, b);
+
+console.log(age1, age2);
+const average = (age1 + age2) / 2;
+console.log(average);
